45d209f5104ffd0d7787c9500a8c13b04eb01cea
Yasin <clkhsyasin@gmail.com>
2022-01-12 19:08:56 +0300

    try-catch files added

---
+++ try-catch/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace try_catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // try
+            // {
+                 
+            // Console.WriteLine("Bir sayi giriniz:");
+            // int sayi = Convert.ToInt32(Console.ReadLine());
+
+            // Console.WriteLine("Girmis oldugunuz sayi :" + sayi);
+            // }
+            // catch(Exception ex){
+            //     Console.WriteLine("Hata:" + ex.Message.ToString());
+            // }
+            // finally
+            // {
+            //     Console.WriteLine("İslem Tamamlandi.");
+            // }
+
+            try
+            {
+                //int a = int.Parse(null);
+                //int a = int.Parse("test");
+                int a = int.Parse("-20000000000000");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Bos deger girdiniz");
+                Console.WriteLine(ex);
+            }
+             catch (FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun degil");
+                Console.WriteLine(ex);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Cok kucuk ya da cok buyuk deger girdiniz..");
+                Console.WriteLine(ex);
+            }
+            finally{
+                Console.WriteLine("İslem basariyla tamamlandi..");
+            }
+
+        }
+    }
+}
